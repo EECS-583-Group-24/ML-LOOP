a03b64d080d93de4401d6677d41bfd43467daf8c
cbangera2 <cbangera@umich.edu>
2023-11-30 19:08:48 -0500

    Working feature collection

--- scripts/collectFeatures.py
+++ scripts/collectFeatures.py
@@ -4,19 +4,14 @@ import subprocess
 # Set the working directory
 directory_path = os.path.join(os.path.dirname(__file__), '../files/simple/')
 csv_file_path = os.path.join(directory_path, 'features.csv')
+final_csv_file_path = os.path.join(directory_path, 'final_features.csv')
 total_features = 33
 
 # Run the Bash script from that location
 script_path = os.path.join(directory_path, 'run.sh')
+os.chdir(directory_path)
 subprocess.run(script_path, shell=True)
-
-# Get the total number of features
-def pad_features(line, total_features):
-    line_features = line.strip().split(',')
-    num_features = len(line_features)
-    if num_features < total_features:
-        line_features.extend(['0'] * (total_features - num_features))
-    return ','.join(line_features)
+os.chdir(os.path.dirname(__file__))
 
 # Open the features.csv file for reading
 with open(csv_file_path, 'r') as file:
@@ -37,6 +32,6 @@ for line in lines:
         file_data[current_file] += line.strip() + ',' if line.strip() else ''
 
 # Write aggregated data to the same features.csv file
-with open(csv_file_path, 'w') as output_file:
+with open(final_csv_file_path, 'w') as output_file:
     for file, data in file_data.items():
         output_file.write(f"{file},{data}\n")
