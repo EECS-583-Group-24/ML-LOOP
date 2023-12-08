eb9bae918a96f9483eaf6546c32b3986a168e72f
Chirag Bangera <cbepic2@gmail.com>
2023-12-08 14:29:35 -0500

    Remove pandas dependency

--- scripts/mergeDataset.py
+++ scripts/mergeDataset.py
@@ -1,8 +1,23 @@
-import pandas as pd
+import csv
 
-df1 = pd.read_csv('final_features.csv')
-df2 = pd.read_csv('best_optimization_results.csv')
+# Read the data from the first CSV file
+with open('final_features.csv', 'r') as f:
+    reader = csv.reader(f)
+    df1 = list(reader)
 
-mergeddf = pd.merge(df1, df2, on='filename')
+# Read the data from the second CSV file
+with open('best_optimization_results.csv', 'r') as f:
+    reader = csv.reader(f)
+    df2 = list(reader)
 
-mergeddf.to_csv('training.csv', index=False)
+# Merge the data based on the 'filename' column
+mergeddf = [df1[0] + df2[0][1:]]  # Start with the headers
+for row1 in df1[1:]:
+    for row2 in df2[1:]:
+        if row1[0] == row2[0]:  # If the 'filename' values match
+            mergeddf.append(row1 + row2[1:])  # Merge the rows
+
+# Write the merged data to a new CSV file
+with open('training.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(mergeddf)
