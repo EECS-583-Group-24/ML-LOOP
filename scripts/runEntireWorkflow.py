69e3527dcac97fdf79cbfc8d8113c3aee69362f3
cbangera2 <cbangera@umich.edu>
2023-12-08 16:15:12 -0500

    Create one script to rule them all

---
+++ scripts/runEntireWorkflow.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+# Directory path to featurePass
+directory_path = os.path.join(os.path.dirname(__file__), '../featurePass/featurePass/')
+
+# Create FeaturePass.so executable
+subprocess.run(['cmake', '..'], cwd=directory_path)
+subprocess.run(['make'], cwd=directory_path)
+
+# Run generatePermutations.py
+subprocess.run(['python3', 'generatePermutations.py'])
+
+# runOptimizations.py - >collectTrainingFeatures.py -> collectTestFeatures.py -> createTraining.py -> createTest.py
+subprocess.run(['python3', 'runOptimizations.py'])
+subprocess.run(['python3', 'collectTrainingFeatures.py'])
+subprocess.run(['python3', 'collectTestFeatures.py'])
+subprocess.run(['python3', 'createTraining.py'])
+subprocess.run(['python3', 'createTest.py'])
+
