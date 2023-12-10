ededb1b20d3a2766c11f7107e6f2c6e701220229
Chirag Bangera <cbepic2@gmail.com>
2023-12-10 16:41:58 -0500

    Update file paths for saving figures

--- mlModel/visualization.py
+++ mlModel/visualization.py
@@ -16,7 +16,7 @@ plt.xlabel('Filenames')
 plt.ylabel('Raw Times')
 plt.title('Raw Times of Files')
 plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.savefig('BarGraph_Files_RawTimes.png', bbox_inches='tight')
+plt.savefig('../figures/BarGraph_Files_RawTimes.png', bbox_inches='tight')
 
 # Plot with ML models as x-axis for raw times
 df.transpose().plot(kind='bar')
@@ -24,7 +24,7 @@ plt.xlabel('ML Models')
 plt.ylabel('Raw Times')
 plt.title('Raw Times of ML Models')
 plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.savefig('BarGraph_Models_RawTimes.png', bbox_inches='tight')
+plt.savefig('../figures/BarGraph_Models_RawTimes.png', bbox_inches='tight')
 
 # Calculate percent change compared to O3 times
 df_o3 = (df.div(df['O3'], axis=0) - 1) * 100
@@ -34,7 +34,7 @@ plt.ylabel('Performance Improvement (%)')
 plt.title('Performance Improvement of Files Over O3')
 plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
 plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.savefig('BarGraph_Files_O3.png', bbox_inches='tight')
+plt.savefig('../figures/BarGraph_Files_O3.png', bbox_inches='tight')
 
 # Plot with ML models as x-axis for percent change compared to O3 times
 df_o3.transpose().plot(kind='bar')
@@ -43,7 +43,7 @@ plt.ylabel('Performance Improvement (%)')
 plt.title('Performance Improvement of ML Models Over O3')
 plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
 plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.savefig('BarGraph_Models_O3.png', bbox_inches='tight')
+plt.savefig('../figures/BarGraph_Models_O3.png', bbox_inches='tight')
 
 # Calculate percent change compared to O2 times
 df_o2 = (df.div(df['O2'], axis=0) -1) * 100
@@ -53,7 +53,7 @@ plt.ylabel('Performance Improvement (%)')
 plt.title('Performance Improvement of Files Over O2')
 plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
 plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.savefig('BarGraph_Files_O2.png', bbox_inches='tight')
+plt.savefig('../figures/BarGraph_Files_O2.png', bbox_inches='tight')
 
 # Plot with ML models as x-axis for percent change compared to O2 times
 df_o2.transpose().plot(kind='bar')
@@ -62,7 +62,7 @@ plt.ylabel('Performance Improvement (%)')
 plt.title('Performance Improvement of ML Models Over O2')
 plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
 plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.savefig('BarGraph_Models_O2.png', bbox_inches='tight')
+plt.savefig('../figures/BarGraph_Models_O2.png', bbox_inches='tight')
 
 # Calculate percent change compared to O1 times
 df_o1 = (df.div(df['O1'], axis=0) -1) * 100
@@ -72,7 +72,7 @@ plt.ylabel('Performance Improvement (%)')
 plt.title('Performance Improvement of Files Over O1')
 plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
 plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.savefig('BarGraph_Files_O1.png', bbox_inches='tight')
+plt.savefig('../figures/BarGraph_Files_O1.png', bbox_inches='tight')
 
 # Plot with ML models as x-axis for percent change compared to O1 times
 df_o1.transpose().plot(kind='bar')
@@ -81,7 +81,7 @@ plt.ylabel('Performance Improvement (%)')
 plt.title('Performance Improvement of ML Models Over O1')
 plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
 plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.savefig('BarGraph_Models_O1.png', bbox_inches='tight')
+plt.savefig('../figures/BarGraph_Models_O1.png', bbox_inches='tight')
 
 # Calculate percent change compared to O0 times
 df_o0 = (df.div(df['O0'], axis=0) -1) * 100
@@ -91,7 +91,7 @@ plt.ylabel('Performance Improvement (%)')
 plt.title('Performance Improvement of Files Over O0')
 plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
 plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.savefig('BarGraph_Files_O0.png', bbox_inches='tight')
+plt.savefig('../figures/BarGraph_Files_O0.png', bbox_inches='tight')
 
 # Plot with ML models as x-axis for percent change compared to O0 times
 df_o0.transpose().plot(kind='bar')
@@ -100,4 +100,4 @@ plt.ylabel('Performance Improvement (%)')
 plt.title('Performance Improvement of ML Models Over O0')
 plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
 plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.savefig('BarGraph_Models_O0.png', bbox_inches='tight')
+plt.savefig('../figures/BarGraph_Models_O0.png', bbox_inches='tight')
