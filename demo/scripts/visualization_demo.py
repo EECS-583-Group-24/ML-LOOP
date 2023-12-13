7775b454054a016e63797ce83b3a7905bc82e673
Chirag Bangera <cbepic2@gmail.com>
2023-12-13 10:18:15 -0500

    Update visualization script in demo

--- demo/scripts/visualization_demo.py
+++ demo/scripts/visualization_demo.py
@@ -9,14 +9,16 @@ os.makedirs("../figures",exist_ok=True)
 path = os.path.join(os.getcwd(), 'results.csv')
 df = pd.read_csv(path)
 figsize = (10, 6)  # Define a variable for the figure size
-include_opts = True  # Include optimization levels in the plots
+include_opts = False  # Include optimization levels in the plots
 
 # Set the index to 'Filename' for easier plotting
-df.set_index('Filename', inplace=True)
+df.set_index('Filename', inplace=True)  
 
 # Plot with filenames as x-axis for raw times
+df2 = df
+df2 = df2.drop(columns=['O0', 'O1', 'O2', 'O3']).iloc[:, :]
 plt.figure(figsize=figsize)  # Use the figure size variable
-df.plot(kind='bar')
+df2.plot(kind='bar')
 plt.xlabel('Filenames')
 plt.ylabel('Raw Times')
 plt.title('Raw Times of Files')
@@ -24,29 +26,20 @@ plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
 plt.savefig('../figures/BarGraph_Files_RawTimes.png', bbox_inches='tight')
 plt.close()
 with open('../figures/BarGraph_Files_RawTimes.md', 'w') as md_file:
-    md_file.write(df.to_markdown())
+    md_file.write(df2.to_markdown())
 
-# Calculate percent change compared to O0, O1, O2, O3 times
-df_o0_o3 = (df.div(df[['O0', 'O1', 'O2', 'O3']].mean(axis=1), axis=0) - 1) * -1
-
-# Calculate mean and median performance improvement for each model
-# Remove the last 4 colums (O0, O1, O2, O3)
-if include_opts:
-    mean_improvement = df_o0_o3.iloc[:, :].mean() * 100
-    median_improvement = df_o0_o3.iloc[:, :].median() * 100
-else:
-    mean_improvement = df_o0_o3.iloc[:, :-4].mean() * 100
-    median_improvement = df_o0_o3.iloc[:, :-4].median() * 100
-
-# Create a new DataFrame for the mean and median improvement
-df_improvement = pd.DataFrame({'Mean': mean_improvement, 'Median': median_improvement})
 
 # Optimization levels to compare against
 optimization_levels = ['O3']
 for opt_level in optimization_levels:
     # Calculate percent change compared to the optimization level
     df_opt = (df.div(df[opt_level], axis=0) - 1) * -1
-
+    
+    if include_opts:
+        df_opt = df_opt.drop(columns=[opt_level]).iloc[:, :]
+    else:
+        df_opt = df_opt.drop(columns=['O0', 'O1', 'O2', 'O3']).iloc[:, :]
+    
     # Calculate percent change compared to O3 times
     plt.figure(figsize=figsize)  # Use the figure size variable
     df_o3 = df_opt * 100
@@ -60,5 +53,33 @@ for opt_level in optimization_levels:
     plt.close()
     with open(f'../figures/BarGraph_Files_{opt_level}.md', 'w') as md_file:
         md_file.write(df_o3.to_markdown())
+        
+    # Remove the optimization level column (O0, O1, O2, O3)
+    if include_opts:
+        mean_improvement = df_opt.drop(columns=[opt_level]).iloc[:, :].mean() * 100
+        median_improvement = df_opt.drop(columns=[opt_level]).iloc[:, :].median() * 100
+    else:
+        mean_improvement = df_opt.iloc[:, :-4].mean() * 100
+        median_improvement = df_opt.iloc[:, :-4].median() * 100
+
+    # Create a new DataFrame for the mean and median improvement
+    df_improvement = pd.DataFrame({'Mean': mean_improvement, 'Median': median_improvement})
+
+    # Plot with ML models as x-axis for mean and median performance improvement
+    plt.figure(figsize=figsize)  # Use the figure size variable
+    df_improvement.plot(kind='bar')
+    plt.xlabel('ML Models')
+    plt.ylabel('Performance Improvement (%)')
+    plt.title(f'Mean and Median Performance Improvement of ML Models Over {opt_level}')
+    plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter()) # Add percent sign to y-axis
+    plt.gca().yaxis.set_minor_locator(mtick.AutoMinorLocator()) # Add minor ticks without labels
+    #plt.grid(True, which='major', axis='y', linestyle='-', color='gray')
+    #plt.gca().set_axisbelow(True)
+    plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5)) # Move legend outside of plot
+    plt.savefig(f'../figures/Total_Models_Improvement_{opt_level}.png', bbox_inches='tight')
+    plt.close() # Close the figure to save memory
+    # Save the DataFrame as a Markdown table in a separate file (need pip3 install tabulate)
+    with open(f'../figures/Total_Models_Improvement_{opt_level}.md', 'w') as md_file:
+        md_file.write(df_improvement.to_markdown())
 
     
